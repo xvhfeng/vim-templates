dea3c4a4c4544ecd4d29a1f604c5365079e6810f
root <root@WIN10-T490>
2023-10-17 15:21:16 +0800

    add c++ file by suffix:cc hh

---
+++ templates/main.cc
@@ -0,0 +1,74 @@
+/********************************************************************
+ *
+ *                        .::::.
+ *                      .::::::::.
+ *                     :::::::::::
+ *                  ..:::::::::::'
+ *               '::::::::::::'
+ *                 .::::::::::
+ *            '::::::::::::::..
+ *                 ..::::::::::::.
+ *               ``:::::::::::::::*
+ *                ::::``:::::::::'        .:::.
+ *               ::::'   ':::::'       .::::::::.
+ *             .::::'      ::::     .:::::::'::::.
+ *            .:::'       :::::  .:::::::::' ':::::.
+ *           .::'        :::::.:::::::::'      ':::::.
+ *          .::'         ::::::::::::::'         ``::::.
+ *      ...:::           ::::::::::::'              ``::.
+ *     ````':.          ':::::::::'                  ::::..
+ *                        '.:::::'                    ':'````..
+ *
+ *                    江城子 . 程序员
+ * 
+ *
+ *               十年生死两茫茫，写程序，到天亮。
+ *                   代码千行，Bug万计藏。
+ *               纵使上线然依样，朝令改，夕发版。
+ * 
+ *               领导废想概无良，蜜自信，虐猿忙。
+ *                   相顾无言，惟有泪千行。
+ *               盏灯键落屏尤亮，朝再起，又通旦。
+ * 
+ * ==================================================================
+ *
+ * You can copy the software or lib under the terms of the gnu
+ * general public license v3,which may be found in the source
+ * kit.
+ *
+ * Warning:
+ *   If use the code for bussiness or any act of gain, you must
+ * get my green light with mail or any documentary evidence.
+ *
+ * ReadMe:
+ *  
+ *
+ * File:        main.cc
+ * Datetime:    2023-10-17 15:19:08 CST
+ * Author:      Seapeak.Xu (www.94geek.com)
+ * Mail:        xvhfeng@126.com
+ * Company:     Copyright(@) Myself
+ *
+ ******************************************************************/
+
+#include "main.h"
+
+/**
+*   this rand number generate by vim plugin: xvhfeng/vim-template in github
+*   and if you use vim,please install this plugin,
+*   but if you use other editor,please generated in file[c,cpp,h,hpp] by yourself
+*   this Rand is allways 32bit number as same as in x64 system
+**/
+//  @@@===--->>> Not Modify This Number <<<---===@@@
+const static unsigned long _MAIN_CC_RAND = 1377382694;
+//  @@@===--->>> Not Modify This Number <<<---===@@@
+
+main::{{CLASS}}()
+{
+    
+}
+
+main::~{{CLASS}}()
+{
+}
+
